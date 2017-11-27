b5b4d5ae8d9eafef439c7a0385be220138e96e25
smalpple <biyong199458@163.com>
2017-11-27 21:00:40 +0800

    basic thread demo practice

--- thread_basic/mtfacfib.py
+++ thread_basic/mtfacfib.py
@@ -1,3 +1,20 @@
 from myThread import MyThread
 from time import ctime,sleep
 
+def fib(x):
+    sleep(0.005)
+    if x < 2:
+        return 1
+    return (fib(x-2) + fib(x-1))
+
+def fac(x):
+    sleep(0.1)
+    if x < 2:
+        return 1
+    return (x * fac(x-1))
+
+def sum(x):
+    sleep(0.1)
+    if x < 2:
+        return 1
+    return (x + sum(x-1))
