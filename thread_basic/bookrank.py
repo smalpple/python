60884b08e73d5836282a4112c822fe160415cdd1
smalpple <biyong199458@163.com>
2017-12-02 21:16:21 +0800

    多线程学习

--- thread_basic/bookrank.py
+++ thread_basic/bookrank.py
@@ -1,7 +1,7 @@
 from atexit import register
 from re import compile
 from threading import Thread
-from time import ctime
+from time import ctime,sleep
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -10,7 +10,9 @@ REGEX = compile('#([\d.]+) in Books')
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"}
 AMZN = 'https://www.amazon.cn/dp/'
 ISBNs = {
-    'B01HZFHPIM':"Python 核心编程(第3版)"
+    'B01HZFHPIM':"Python 核心编程(第3版)",
+    'B01I2JGBMQ':"编程小白的第一本 Python 入门书",
+    'B01M68PABD':"Python编程快速上手 让繁琐工作自动化"
 }
 
 def getRanking(isbn):
@@ -19,15 +21,18 @@ def getRanking(isbn):
     # print(soup)
     # #print(soup.find_all(compile("Kindle商店商品里排第\d,\d\d\d名")))
     #print(re.findall("Kindle商店商品里排第\S*名",soup.text))
-    return re.findall("Kindle商店商品里排第\S*名",soup.text)
+    return ('').join(re.findall("排第\S*名",soup.text))
 
 def _showRanking(isbn):
-    print('- %r ranked %s' %(ISBNs[isbn],getRanking(isbn)))
+    print('- %r%s' %(ISBNs[isbn],getRanking(isbn)))
+    sleep(2)
+
 
 def _main():
     print('At',ctime(),'on Amazon...')
     for isbn in ISBNs:
-        _showRanking(isbn)
+        #_showRanking(isbn)                   ### 普通的方式
+        Thread(target=_showRanking,args=(isbn,)).start()      ### 多线程的方式
 
 @register
 def _atexit():
