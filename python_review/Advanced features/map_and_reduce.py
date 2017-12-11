a9ffe443bef772b275c9b5722fe32b93e203ec87
smalpple <biyong199458@163.com>
2017-12-11 18:09:19 +0800

    basic thread demo practice

--- python_review/Advanced features/map_and_reduce.py
+++ python_review/Advanced features/map_and_reduce.py
@@ -13,7 +13,16 @@ def str2int(s):
 def normalize(name):
     return name[0].upper()+name[1:].lower()
 
-if __name__ == '__main__':
-    L1 = ['adam', 'LISAss', 'barT']
-    L2 = list(map(normalize, L1))
-    print(L2)
+L1 = ['adam', 'LISAss', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+### Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+def prod(L):
+    return reduce(lambda x,y:x*y,L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
