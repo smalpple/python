bef4e5f6f7a75daf7502148c462516be577f4c01
smalpple <biyong199458@163.com>
2018-07-28 20:48:57 +0800

    scrapy project

--- fenye_test.py
+++ fenye_test.py
@@ -1,39 +1,8 @@
 from json import *
-res_json = {
-	"code": 200,
-	"status": "success",
-	"message": "操作成功",
-	"data": {
-		"array": [{
-			"createTime": "2018-06-26 14:33:18",
-			"recashName": "升级合伙人奖励（一级）",
-			"recashFee": 3,
-			"inviteeName": "148****8884"
-		}, {
-			"createTime": "2018-06-26 14:31:51",
-			"recashName": "升级合伙人奖励（一级）",
-			"recashFee": 3,
-			"inviteeName": "148****8884"
-		}, {
-			"createTime": "2018-06-26 14:29:37",
-			"recashName": "升级合伙人奖励（一级）",
-			"recashFee": 3,
-			"inviteeName": "148****8884"
-		}, {
-			"createTime": "2018-06-26 14:29:09",
-			"recashName": "升级合伙人奖励（一级）",
-			"recashFee": 3,
-			"inviteeName": "148****8884"
-		}, {
-			"createTime": "2018-06-26 14:26:53",
-			"recashName": "升级合伙人奖励（一级）",
-			"recashFee": 3,
-			"inviteeName": "148****8884"
-		}]
-	}
-}
+import clipboard
+res_json = {"message":"查询成功","status":"success","data":{"array":[{"fee":2,"downloadCount":47,"vipFee":1,"authorName":"孙庆新@微商水印相的","courseid":23,"vipFree":0,"courseSize":862,"courseType":2,"courseIcon":"https://resourcessl.edujia.com/ws/vip/courseware/icon/word_icon.png","introduction":"本次测试报告为UEM4.4的性能测试总结报告，目的在于总结性能测试工作，并分析测试结果，描述是否符合性能需求。预期参考人员包括用户、测试人员、开发人员、项目管理者、质量管理人员和需要阅读本报告的高层管理人员。","courseName":"性能测试报告性能测试报告性能测试报告性能测试报告"}]},"code":200}
 date_array = res_json['data']['array']
-date_finallen = 30
+date_finallen = 5
 date_len = len(date_array)
 if date_len <= date_finallen:
     date_end = date_finallen - date_len
@@ -43,6 +12,8 @@ if date_len <= date_finallen:
     res_json['data']['array'] = array
     json_res = dumps(res_json, ensure_ascii=False)
     print(json_res)
+    clipboard.copy(json_res)
+
 else:
 	print("达到分页标准，不需要生成")
 
---
+++ scrapy系列/mingyan/mingyan/spiders/item_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class itemSpider(scrapy.Spider):
+    name = 'itemSpider'
+    start_urls = ['http://lab.scrapyd.cn']
+
+    def parse(self, response):
+
+        mingyan = response.css('div.quote')
+
+        for i in range(len(mingyan)):
+            text = mingyan[i].css('.text::text').extract_first()
+            author = mingyan[i].css('.author::text').extract_first()
+            tags = ','.join(mingyan.css('.tags .tag::text').extract())
+
+            filename = '{}-语录.txt'.format(author)
+            f = open(filename,"a+")
+            f.write(text)
+            f.write('\n')
+            f.write('标签：'+tags)
+            f.close()
